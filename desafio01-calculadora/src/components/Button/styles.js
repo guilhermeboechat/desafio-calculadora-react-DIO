eaed02b607918f1e13a162c8c0556ab5188ca185
guilhermeboechat <guilhermeboechatdf@hotmail.com>
2024-11-08 12:20:59 -0300

    startingsdasdasdas

---
+++ desafio01-calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 0.5px solid lightgrey;
+    background-color: lightergrey;
+    color: black;
+    font-size: 24px;
+    font-weight: 700px;
+    flex: 1;
+    &:hover{
+        opacity: 0.4;
+        width: 1px;
+    }
+`
